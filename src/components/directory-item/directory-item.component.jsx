341f71389de1bf2e11150a23f662bab0b4eb135e
boodscode237 <boodscode@gmail.com>
2022-09-22 13:14:05 +0500

    added styled components

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,3 +1,4 @@
+import {Link, useNavigate} from "react-router-dom";
 import {
   BackgroundImage,
   Body,
@@ -5,13 +6,15 @@ import {
 } from './directory-item.styles';
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate()
+    const onNavigateHandler = () => navigate(route)
   return (
     <DirectoryItemContainer>
-      <BackgroundImage imageUrl={imageUrl} />
+      <BackgroundImage onClick={onNavigateHandler} imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
-        <p>Shop Now</p>
+        <p><Link to={`${route}`}>Shop Now</Link></p>
       </Body>
     </DirectoryItemContainer>
   );
